8ebf6d3972082e680357e65b7ebd024d760a4477
Luisa Napier <luisanapier@Luisas-MacBook-Pro.local>
2023-05-17 20:14:50 -0700

    play again function working

--- game.py
+++ game.py
@@ -4,9 +4,9 @@
 import random
 
 guest = input("Welcome to the Guessing game. What's your name? ")
-player_name = input(f"{guest}, I'm thinking of a number between 1 and 100. ")
+player_name = print(f"{guest}, I'm thinking of a number between 1 and 100. ")
 
-
+score = 0
 numbers_guesses = random.randint(1,100)
 
 while True:
@@ -16,8 +16,15 @@ while True:
     if guess != numbers_guesses:
         if guess < numbers_guesses:
             print("Your guess is too low. Try again.")
-
+            score += 1 
         elif guess > numbers_guesses:
             print("Your guess is too high. Try again.")
+            score += 1
     else:
-        print(f"Well done {guest}! You got it right!")
+        print(f"Well done {guest}! You got it right in {score} guesses!")
+        play_again = input("Would you like to play again? y/n ")
+        score = 0
+        if play_again == "yes":
+            continue
+        else:
+            break
