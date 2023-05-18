792e194a227f9525f400b3a6a9fe87f5d853b3b7
Luisa Napier <luisanapier@Luisas-MacBook-Pro.local>
2023-05-17 19:54:22 -0700

    Hint function working

--- game.py
+++ game.py
@@ -4,6 +4,20 @@
 import random
 
 guest = input("Welcome to the Guessing game. What's your name? ")
-guess = input(f"{guest}, I'm thinking of a number between 1 and 100. Can you guess? ")
-guess = int(guess)
+player_name = input(f"{guest}, I'm thinking of a number between 1 and 100. ")
 
+
+numbers_guesses = random.randint(1,100)
+
+while True:
+    guess = input("Try to guess my number. ")
+    guess = int(guess)
+
+    if guess != numbers_guesses:
+        if guess < numbers_guesses:
+            print("Your guess is too low. Try again.")
+
+        elif guess > numbers_guesses:
+            print("Your guess is too high. Try again.")
+    else:
+        print(f"Well done {guest}! You got it right!")
